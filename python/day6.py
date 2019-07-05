36f413f95873eee095a684ae6f23e0080772e44c
lclc <liuchao7@jd.com>
2019-07-05 20:07:03 +0800

    增加函数和字符串操作

---
+++ python/day6.py
@@ -0,0 +1,28 @@
+# 可变参数
+# 在参数名前面的*表示args是一个可变参数
+# 即在调用add函数时可以传入0个或多个参数
+def add(*args):
+    total = 0
+    for val in args:
+        total += val
+    return total
+
+print(add(1,2,3,4,5))
+
+def foo():
+    b = 'hello'
+
+    def bar():  # Python中可以在函数内部再定义函数
+        c = True
+        print(a)
+        print(b)
+        print(c)
+
+    bar()
+    # print(c)  # NameError: name 'c' is not defined
+
+
+if __name__ == '__main__':
+    a = 100
+    # print(b)  # NameError: name 'b' is not defined
+    foo()
